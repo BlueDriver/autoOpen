c4ae55d4e04d7b369d9fc22fe3eb6fe080cc43d4
BlueDriver <2943884288@qq.com>
2018-12-18 19:27:10 +0800

    [add] first commit

---
+++ autoOpen/scripts/contentscript.js
@@ -0,0 +1,5 @@
+console.log("autoOpen ------------------------------------------------------------------")
+document.getElementById('article_content').style.overflow = "scroll";
+document.getElementById('article_content').style.height = "auto";
+document.getElementsByClassName('hide-article-box')[0].style.paddingTop = "0px";
+console.log("autoOpen ------------------------------------------------------------------")
