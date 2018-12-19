97b871d3ebb1205851e94c84092f7fca03adec21
BlueDriver <2943884288@qq.com>
2018-12-19 09:14:40 +0800

    [feat]::: 去掉滚动条和【阅读更多】按钮

--- autoOpen/scripts/contentscript.js
+++ autoOpen/scripts/contentscript.js
@@ -1,5 +1,10 @@
 console.log("autoOpen ------------------------------------------------------------------")
-document.getElementById('article_content').style.overflow = "scroll";
+//文章超长后滚动
+//document.getElementById('article_content').style.overflow = "scroll";
+//文章高度为auto
 document.getElementById('article_content').style.height = "auto";
+//去掉渐变遮罩
 document.getElementsByClassName('hide-article-box')[0].style.paddingTop = "0px";
+//隐藏阅读更多的按钮
+document.getElementsByClassName('hide-article-box')[0].style.display='none'
 console.log("autoOpen ------------------------------------------------------------------")
