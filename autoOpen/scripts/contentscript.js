55aa106ff8bd42048add1afe571c077da41316eb
BlueDriver <2943884288@qq.com>
2018-12-19 10:31:05 +0800

    [feat] 增加一个toast弹窗

--- autoOpen/scripts/contentscript.js
+++ autoOpen/scripts/contentscript.js
@@ -1,4 +1,6 @@
-console.log("autoOpen ------------------------------------------------------------------")
+
+console.log("autoOpen ------------------------------------------------------------------ start")
+
 //文章超长后滚动
 //document.getElementById('article_content').style.overflow = "scroll";
 //文章高度为auto
@@ -9,4 +11,20 @@ document.getElementsByClassName('hide-article-box')[0].style.paddingTop = "0px";
 document.getElementsByClassName('hide-article-box')[0].style.display='none';
 //隐藏底部登录框
 document.getElementsByClassName('pulllog-box')[0].style.display='none';
-console.log("autoOpen ------------------------------------------------------------------")
+//toast function
+function Toast(msg,duration){
+  duration=isNaN(duration)?3000:duration;
+  var m = document.createElement('div');
+  m.innerHTML = msg;
+  m.style.cssText="max-width:60%;min-width: 150px;padding:0 14px;height: 40px;color: rgb(255, 255, 255);line-height: 40px;text-align: center;border-radius: 4px;position: fixed;top: 80%;left: 50%;transform: translate(-50%, -50%);z-index: 999999;background: rgba(0, 0, 0,.7);font-size: 26px;";
+  document.body.appendChild(m);
+  setTimeout(function() {
+    var d = 0.5;
+    m.style.webkitTransition = '-webkit-transform ' + d + 's ease-in, opacity ' + d + 's ease-in';
+    m.style.opacity = '0';
+    setTimeout(function() { document.body.removeChild(m) }, d * 1000);
+  }, duration);
+}
+
+Toast("autoOpen", 3000);
+console.log("autoOpen ------------------------------------------------------------------ end")
